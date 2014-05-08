a5549cbde4289c233520b10aeb58d80589be0383
Takashi AOKI <federkasten@me.com>
2014-05-08 04:51:38 -0700

    migrate some utility functions

--- src/kormas.clj
+++ src/kormas.clj
@@ -1,4 +1,8 @@
-(ns kormas)
+(ns kormas
+  (:require [clojure.java.io :as io]
+            [clojure.string :as cstr]
+            (korma [core :refer :all]
+                   [db :refer :all])))
 
 (def ^:private db-test-connection
   {:test-connection-query "SELECT 1"
@@ -18,10 +22,22 @@
        nil)))
 
 (defn db-config
-  [& {:keys [user password host db]}]
+  [{:keys [user password host db]}]
   (merge {:user user
           :password password
           :host host
           :db db}
           db-test-connection
           db-option))
+
+(defn- read-sql
+  [f]
+  (let [raw-sql (slurp f)]
+    (filter #(not (nil? (re-find #"\w+" %))) (cstr/split raw-sql #";"))))
+
+(defn exec-sql-into-db
+  [f db]
+  (with-db db
+    (doseq [sql (read-sql (io/resource f))]
+      (exec-raw (get-connection db) [sql])))
+  nil)
