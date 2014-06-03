2f68b65a80fa233e241ebf9163e1eaefb8292bb7
Takashi AOKI <federkasten@me.com>
2014-06-02 23:16:48 -0700

    add utility function 'transform-key'

--- src/kormas.clj
+++ src/kormas.clj
@@ -42,8 +42,20 @@
       (exec-raw (get-connection db) [sql])))
   nil)
 
+;;;
+;;; Utility
+;;;
+
 (defmacro swap
   [m k f]
   `(if-not (nil? (get ~m ~k))
      (assoc ~m ~k (~f (get ~m ~k)))
      ~m))
+
+(defn transform-key
+  [v k new-k]
+  (if (contains? v k)
+    (-> v
+        (assoc new-k (k v))
+        (dissoc k))
+    v))
