6882dd07a71d0face55640ec2c04252b9c13f4c1
Takashi AOKI <federkasten@me.com>
2015-05-22 18:26:19 +0900

    remove dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/federkasten/kormas"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [korma "0.4.1"]
-                 [mysql/mysql-connector-java "5.1.35"]
-                 [log4j "1.2.17" :exclusions [javax.mail/mail
-                                              javax.jms/jms
-                                              com.sun.jdmk/jmxtools
-                                              com.sun.jmx/jmxri]]])
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [korma "0.4.1"]
+                                  [mysql/mysql-connector-java "5.1.35"]
+                                  [log4j "1.2.17" :exclusions [javax.mail/mail
+                                                               javax.jms/jms
+                                                               com.sun.jdmk/jmxtools
+                                                               com.sun.jmx/jmxri]]]}})
