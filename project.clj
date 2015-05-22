048a7618236762c0fb86c5a972b530c890a04e03
Takashi AOKI <federkasten@me.com>
2015-05-22 21:10:44 +0900

    version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kormas "0.1.0-SNAPSHOT"
+(defproject kormas "0.1.0"
   :description "Utility functions for Korma (A Clojure Library for Tasty SQL)"
   :url "https://github.com/federkasten/kormas"
   :license {:name "Apache License, Version 2.0"
